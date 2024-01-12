5d7004afe0d7ed8e2f1e4b0e20f46800c48f80a5
sukk <suk.kwakk@gmail.com>
2024-01-12 19:56:58 +0900

    Feat: Auth 페이지 구현

--- src/components/CommentItem/InputBox/index.tsx
+++ src/components/CommentItem/InputBox/index.tsx
@@ -4,13 +4,13 @@ import './style.css'
 //          interface: Input Box 컴포넌트 Properties          //
 interface Props {
   label: string;
-  type: 'text' | 'passward';
+  type: 'text' | 'password';
   placeholder: string;
   value: string;
   setValue: Dispatch<SetStateAction<string>>;
   error: boolean;
 
-  icon?: string;
+  icon?: 'eye-light-off-icon' | 'eye-light-on-icon' | 'expand-right-light-icon';
   onButtonClick?: () => void;
 
   message?: string;
@@ -46,8 +46,8 @@ const InputBox = forwardRef<HTMLInputElement, Props>((props: Props, ref) => {
       <div className={error? 'inputbox-container-error': 'inputbox-container'}>
         <input ref={ref} type={type} className='input' placeholder={placeholder} value={value} onChange={onChangeHandler} onKeyDown={onKeyDown}/>
         {onButtonClick !== undefined && (
-          <div className='icon-button'>
-            {icon !== undefined && <div className='icon ${icon}'></div>}
+          <div className='icon-button' onClick={onButtonClick}>
+            {icon !== undefined && <div className={`icon ${icon}`}></div>}
           </div>
         )}
         
