bfecfcad0f26ed8846226daeb846bfff60a8a88b
sukk <suk.kwakk@gmail.com>
2024-01-19 11:03:47 +0900

    Feat: 회원가입 및 로그인 API와 Auth 화면 연동

--- src/views/Authentication/index.tsx
+++ src/views/Authentication/index.tsx
@@ -1,6 +1,13 @@
-import React, { useState, KeyboardEvent, useRef } from 'react'
+import React, { useState, KeyboardEvent, useRef, ChangeEvent } from 'react'
 import './style.css'
 import InputBox from 'components/CommentItem/InputBox';
+import { SignInRequestDto } from 'apis/request/auth';
+import { signInRequest } from 'apis';
+import { SignInResponseDto } from 'apis/response/auth';
+import { ResponseDto } from 'apis/response';
+import { useCookies } from 'react-cookie';
+import { MAIN_PATH } from 'constant';
+import { useNavigate } from 'react-router-dom';
 
 //          component: 인증 화면 컴포넌트          //
 export default function Authentication() {
@@ -8,6 +15,11 @@ export default function Authentication() {
   //          state: 화면 상태          //
   const [view, setView] = useState<'sign-in' | 'sign-up'>('sign-in');
 
+  //          state: 화면 상태          //
+  const [cookies, setCookies] = useCookies();
+
+  //          function: 네비게이트 함수          //
+  const navigator = useNavigate();
 
   //          component: sign in card 컴포넌트          //
   const SignInCard = () => {
@@ -27,9 +39,45 @@ export default function Authentication() {
     //          state: error 상태          //
     const [error, setError] = useState<boolean>(false);
     
+
+    //          function: sign in response 처리 함수          //
+    const signInResponse = (responseBody: SignInResponseDto | ResponseDto | null) => {
+      if (!responseBody) {
+        alert('네트워크 이상입니다.')
+        return;
+      }
+      const { code } = responseBody;
+      if (code === 'DBE') alert('데이터베이스 오류입니다.');
+      if (code === 'SF' || code === 'VF') setError(true);
+      if (code !== 'SU') return;
+
+      const { token, expirationTime } = responseBody as SignInResponseDto;
+      const now = new Date().getTime();
+      const expires = new Date(now + expirationTime * 1000);
+      
+      setCookies('accesToken', token, {expires, path: MAIN_PATH()})
+      navigator(MAIN_PATH())
+    }
+    
+
+    //          event handler: 이메일 변경 이벤트 처리          //
+    const onEmailChangeHanlder = (event: ChangeEvent<HTMLInputElement>) => {
+      setError(false);
+      const { value } = event.target;
+      setEmail(value)
+    }
+
     //          event handler: 로그인 버튼 클릭 이벤트 처리          //
     const onSignInButtonClickHandler = () => {
+      const requestBody: SignInRequestDto = { email, password };
+      signInRequest(requestBody).then(signInResponse)
+    }
 
+    //          event handler: 비밀번호 변경 이벤트 처리          //
+    const onPasswordChangeHanlder = (event: ChangeEvent<HTMLInputElement>) => {
+      setError(false);
+      const { value } = event.target;
+      setPassword(value)
     }
 
     //          event handler: 회원가입 링크 클릭 이벤트 처리          //
@@ -68,8 +116,8 @@ export default function Authentication() {
               <div className='auth-card-title'>{'로그인'}</div>
               <div></div>
             </div>
-            <InputBox ref={emailRef} label='이메일 주소' type='text' placeholder='이메일 주소를 입력해주세요.' error={error} value={email} setValue={setEmail} onKeyDown={onEmailKeyDownHandler} />
-            <InputBox ref={passwordRef} label='패스워드' type={passwordType} placeholder='비밀번호를 입력해주세요.' error={error} value={password} setValue={setPassword} icon={passwordButtonIcon} onButtonClick={onPasswordButtonClickHandler} onKeyDown={onPasswordKeyDownHandler}/>
+            <InputBox ref={emailRef} label='이메일 주소' type='text' placeholder='이메일 주소를 입력해주세요.' error={error} value={email} onChange={onEmailChangeHanlder} onKeyDown={onEmailKeyDownHandler} />
+            <InputBox ref={passwordRef} label='패스워드' type={passwordType} placeholder='비밀번호를 입력해주세요.' error={error} value={password} onChange={onPasswordChangeHanlder} icon={passwordButtonIcon} onButtonClick={onPasswordButtonClickHandler} onKeyDown={onPasswordKeyDownHandler}/>
           </div>
           <div className='auth-card-bottom'>
             {error && 
